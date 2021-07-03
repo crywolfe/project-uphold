6fb098b0b3b9aa4c1133b7bc30e1f50745b3a677
Gerry Wolfe <gwolfe@slync.io>
2021-07-03 17:44:00 -0500

    handle multiple currency pairs and expand arguments

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,25 @@
 import express from 'express';
 import axios, { AxiosResponse } from 'axios';
-// import {priceChange2} from './service/service';
-import worker from 'worker_threads'
 import inquirer from 'inquirer';
 // maybe use ramda for fx programming compose...
 
-const DELAY = 5000
+interface IAsk {
+    pair: string;
+    askPrice: number
+}
+
+interface IOscillation {
+    pair: string;
+    oscillation: number
+}
 
 const app = express();
 const PORT = 3000;
 
-let previousAsk = 0;
-let response;
-
-let pairs = [];
+const previousAsks: Map<string, IAsk> = new Map();
+const currentAsks: Map<string, IAsk> = new Map();
+const oscillations: Map<string, IOscillation> = new Map();
+const pairs: string[] = [];
 
 
 app.get('/', (req, res) => {
@@ -29,22 +35,34 @@ const questions = [
         name: 'pair',
         type: 'input',
         message: 'Enter the pair as XXX-XXX, e.g. BTC-USD: ',
+        default: 'BTC-USD',
         validate: (pair: string) => {
             const pairValidated = /^([A-Z]){3}[-]([A-Z]){3}/.test(pair);
-            console.info(`\n Please re-enter in a validate format.\n`);
+            if (!pairValidated) {
+                console.info(`\n Please re-enter in a validate format.\n`);
+            }
             return pairValidated;
         }
     },
     {
-        name: 'oscillationPercentage',
+        name: 'oscillation',
         type: 'number',
         message: 'What percentage change would you like to be notified?, default is .01% ',
-        default: .0001,
-        validate: (oscillationPercentage: number) => {
-            const numberValidated = /^\d{0,10}(\.\d{1,4})?/.test(oscillationPercentage.toString());
-            console.info(`\n Please re-enter in a valid format such as 0.XX, .X, X.\n`);
+        default: .01,
+        validate: (oscillation: number) => {
+            const numberValidated = /^\d{0,10}(\.\d{1,4})?/.test(oscillation.toString());
+            if (!numberValidated) {
+                console.info(`\n Please re-enter in a valid format such as 0.XX, .X, X.\n`);
+            }
             return numberValidated;
         }
+    },
+    {
+        name: 'interval',
+        type: 'number',
+        message: 'How often do you want prices retrieved? \n' +
+        '(Must be greater than or equal to 5 seconds and only the latest value is used), default is 5',
+        default: 5
     },
     {
         name: 'askAgain',
@@ -59,79 +77,78 @@ const setPair = (): void => {
     
     inquirer.prompt(questions).then((answers) => {
         pairs.push(answers.pair);
+        oscillations[answers.pair] = answers.oscillation
         if (answers.askAgain) {
             setPair();
         } else {
             getInitialRequest(pairs);
-            // getRequest(pairs, answers.oscillationPercentage/100);
-            // priceChange2;
-    
-            // runInterval(pairs, answers.oscillationPercentage/100);
+            getRequest(pairs, oscillations);
+            runInterval(pairs, oscillations, answers.interval);
         }
     });
 }
 setPair();
 
-interface IRequest {
-    pair: string,
-    request: Promise<AxiosResponse<any>>
-}
 const getInitialRequest = async (pairs: string[]): Promise<void> => {
-    const requests: Promise<AxiosResponse<any>>[] = [];
-    // const requests2: IRequest[] = []
-    // const requestMap: Map<string, Promise<AxiosResponse<any>>> = new Map();
+    
+    const requests = buildRequests(pairs);
 
-    pairs.forEach((pair: string) => {
-        // requestMap[pair] = axios.get(`https://api.uphold.com/v0/ticker/${pair}`)
-        // requests2.push({pair, request: axios.get(`https://api.uphold.com/v0/ticker/${pair}`)})
-        requests.push(axios.get(`https://api.uphold.com/v0/ticker/${pair}`))
-
-    })
-    const responses = await axios.all(requests).then(axios.spread((...responses) => {
+    await axios.all(requests).then(axios.spread((...responses) => {
         responses.forEach((response) => {
-            const pairNameFromPath = response.request.path.slice(-7);
-            console.log({responseData: response.data})
-            console.log(`initial price for ${pairNameFromPath} is ${+response.data.ask}`);
+            const pairName = response.request.path.slice(-7);
+
+            currentAsks[pairName] = +response.data.ask;
+            previousAsks[pairName] = currentAsks[pairName]
 
         })
     })).catch(err => {
         console.log(err);
     });
-    // const res = await reqAll;
-    // const req = axios.get(`https://api.uphold.com/v0/ticker/${pair}`);
-    // const res = await req;
-    // response = res.data;
-
-    // const currentAsk = +response.ask;
-    // previousAsk = currentAsk;
 
 }
 
-const getRequest = async (pair: string, oscillationPercentage: number): Promise<void> => {
-    const req = axios.get(`https://api.uphold.com/v0/ticker/${pair}`);
-    const res = await req;
+const getRequest = async (pairs: string[], oscillations: Map<string, IOscillation>): Promise<void> => {
+
+    const requests = buildRequests(pairs);
     
-    console.info(res.data);
-    response = res.data;
-    const currentAsk = +response.ask;
+    await axios.all(requests).then(axios.spread((...responses) => {
+        responses.forEach((response) => {
+            const pairName = response.request.path.slice(-7);
 
-    priceChange(previousAsk, currentAsk, oscillationPercentage);
+            currentAsks[pairName] = +response.data.ask;
+            
+            priceChange(pairName, previousAsks[pairName], currentAsks[pairName], oscillations[pairName]);
+            
+            previousAsks[pairName] = currentAsks[pairName]
 
-    previousAsk = currentAsk;
+        })
+    })).catch(err => {
+        console.log(err);
+    });
 }
 
-const priceChange = ((previousAsk: number, currentAsk: number, oscillationPercentage: number): void => {
-    if (currentAsk > previousAsk && currentAsk / previousAsk > oscillationPercentage/100) {
-        console.info(`Alert - Price Change of Ask Up ⬆️ than ${oscillationPercentage*100}% from ${previousAsk} to ${currentAsk}`);
-    } else if (currentAsk < previousAsk && previousAsk / currentAsk > oscillationPercentage/100) {
-        console.info(`Alert - Price Change of Ask Down ⬇️ more than ${oscillationPercentage*100}% from ${previousAsk} to ${currentAsk}`);
+const priceChange = ((pairName: string, previousAsk: number, currentAsk: number, oscillation: number): void => {
+    if (currentAsk > previousAsk && Math.abs(1 - currentAsk / previousAsk) > oscillation/100) {
+        console.info(`Alert - Price Change of ${pairName} Ask Up ⬆️ more than ${oscillation}% from ${previousAsk} to ${currentAsk}`);
+    } else if (currentAsk < previousAsk && Math.abs(1 - previousAsk / currentAsk) > oscillation/100) {
+        console.info(`Alert - Price Change of ${pairName} Ask Down ⬇️ more than ${oscillation}% from ${previousAsk} to ${currentAsk}`);
     }
 })
 
-const runInterval = (pair: string, oscillationPercentage: number): void => {
+const runInterval = (pairs: string[], oscillations: Map<string, IOscillation>, interval: number): void => {
+    const intervalInMS = interval*1000;
     setInterval(() => {
-        getRequest(pair, oscillationPercentage);
-    }, DELAY)
+        getRequest(pairs, oscillations);
+    }, intervalInMS)
 };
 
-export default app;
+const buildRequests = (pairs: string[]): Promise<AxiosResponse<any>>[] => {
+    const requests: Promise<AxiosResponse<any>>[] = [];
+
+    pairs.forEach((pair: string) => {
+        requests.push(axios.get(`https://api.uphold.com/v0/ticker/${pair}`));
+
+    });
+    return requests;
+}
+
